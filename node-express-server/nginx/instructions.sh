da41c35c0432697311f2230d95aa5631188a9786
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2018-01-14 19:18:48 +0100

    chore(): add licenses

--- node-express-server/nginx/instructions.sh
+++ node-express-server/nginx/instructions.sh
@@ -1,2 +1,5 @@
+# Taken from https://gist.github.com/plentz/6737338
+# @plentz thank u for you amazing work :)
+
 # to generate your dhparam.pem file, run in the terminal
 openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
